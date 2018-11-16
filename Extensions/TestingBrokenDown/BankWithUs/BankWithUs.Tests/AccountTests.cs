d715082f5d50ed3d891125096b267ba29d3d4682
lisawiththegoodcode <lisa.umhey@gmail.com>
2018-11-15 21:11:10 -0600

    end of clas 11.15

--- Extensions/TestingBrokenDown/BankWithUs/BankWithUs.Tests/AccountTests.cs
+++ Extensions/TestingBrokenDown/BankWithUs/BankWithUs.Tests/AccountTests.cs
@@ -10,11 +10,11 @@ namespace BankWithUs.Tests
     public class AccountTests
     {
         [Fact]
-        public async Task Test_Account_Create()
+        public async Task Test_Account_Create() //this could be void
         {
             AccountsController controller = new AccountsController();
 
-            IActionResult result = await controller.Create(new Account() { Name = "Test", SSN = "078-05-1120" });
+            IActionResult result = await controller.Create(new Account() { Name = "Test", SSN = "078-05-1120" }); //if not an async task, get rid of await and put .result on the end
             Assert.True(result is ViewResult);
             var response = (result as ViewResult);
             var responseItem = response.Model as Account;
